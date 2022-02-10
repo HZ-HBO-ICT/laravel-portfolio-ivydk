24a3394070b316931c487089d53d2cc16a520b60
ivydk <dekk0123@hz.nl>
2022-02-10 16:37:27 +0100

    checks if there is a link to show

--- resources/views/pages/faq.blade.php
+++ resources/views/pages/faq.blade.php
@@ -6,14 +6,18 @@
     <h1>Veelgestelde vragen</h1>
         @foreach($posts as $post)
             <details class="bottom">
+
                 <summary>{{ $post->question }}</summary>
                 <p>
                     {{$post->answer}}
                 </p>
-            {{--TODO: check if there is a link--}}
-                <p>
-                    <a href="{{ $post->link }}">{{$post->link}}</a>
-                </p>
+
+                    @if($post->link != null)
+                        <p>
+                            <a href="{{ $post->link }}">{{$post->link}}</a>
+                        </p>
+                    @endif
+
             </details>
         @endforeach
 
