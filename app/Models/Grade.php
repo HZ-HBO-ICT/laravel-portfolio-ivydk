74241fec9b376899a9b3d72ad601444bb97e3157
ivydk <dekk0123@hz.nl>
2022-02-10 17:15:16 +0100

    fixed errors

--- app/Models/Grade.php
+++ app/Models/Grade.php
@@ -9,9 +9,17 @@ class Grade extends Model
 {
     use HasFactory;
 
+    /**
+     * checks if the new result >= current grade
+     * when the grade is >= current grade the new grade becomes the best_grade and
+     * the passes_at becomes the current day
+     *
+     * @param $newGrade
+     * @return void
+     */
     public function addResult($newGrade): void
     {
-        if($newGrade >= $this->best_grade)
+        if ($newGrade >= $this->best_grade) 
         {
             $this->best_grade = $newGrade;
             $this->passed_at = now();
